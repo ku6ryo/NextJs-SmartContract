3dbcfb19d67a87f4d4c61d8722e23b5ba070f3e1
ku6ryo <ku6ryo@gmail.com>
2022-07-31 15:11:10 +0900

    updated

--- server/pages/index.tsx
+++ server/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 import axios from 'axios'
 import Definition from "../contract/SecureMintingNFT.json"
 import Web3 from "web3"
@@ -15,6 +14,9 @@ const Home: NextPage = () => {
 
   const [initializing, setInitializing] = useState(true)
   const [nftName, setNftName] = useState("")
+  const [mintMessage, setMintMessage] = useState("")
+  const [ownerAddress, setOwnerAddress] = useState("")
+  const [addressToCheck, setAddressToCheck] = useState("")
 
   useEffect(() => {
     ;(async () => {
@@ -36,18 +38,43 @@ const Home: NextPage = () => {
   const mint = async () => {
     const { NEXT_PUBLIC_CONTRACT_ADDRESS } = process.env
     if (window.ethereum && NEXT_PUBLIC_CONTRACT_ADDRESS) {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts"
-      })
-      const account = accounts[0]
-      const res = await axios.get("/api/getTokenIdAndSign?address=" + account)
-      const {
-        tokenId,
-        signature,
-      } = res.data
-      const web3 = new Web3(window.ethereum)
-      const contract = new web3.eth.Contract(Definition.abi as any, NEXT_PUBLIC_CONTRACT_ADDRESS)
-      await contract.methods.mint(tokenId, signature).send({ from: account })
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts"
+        })
+        const account = accounts[0]
+        const res = await axios.get("/api/getTokenIdAndSign?address=" + account)
+        const {
+          tokenId,
+          signature,
+        } = res.data
+        const web3 = new Web3(window.ethereum)
+        const contract = new web3.eth.Contract(Definition.abi as any, NEXT_PUBLIC_CONTRACT_ADDRESS)
+        await contract.methods.mint(tokenId, signature).send({ from: account })
+        setMintMessage("Minted - token ID: " + tokenId)
+      } catch (e) {
+        console.error(e)
+        setMintMessage("Mint failed")
+      }
+    }
+  }
+
+  const checkOwner = async () => {
+    const { NEXT_PUBLIC_CONTRACT_ADDRESS } = process.env
+    if (window.ethereum && NEXT_PUBLIC_CONTRACT_ADDRESS) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts"
+        })
+        const account = accounts[0]
+        const web3 = new Web3(window.ethereum)
+        const contract = new web3.eth.Contract(Definition.abi as any, NEXT_PUBLIC_CONTRACT_ADDRESS)
+        const address = await contract.methods.ownerOf(addressToCheck).call({ from: account })
+        setOwnerAddress(address)
+      } catch (e) {
+        console.error(e)
+        setMintMessage("Mint failed")
+      }
     }
   }
 
@@ -63,6 +90,10 @@ const Home: NextPage = () => {
       <div>
         <button onClick={mint}>mint</button>
       </div>
+      <div>{mintMessage}</div>
+      <div><input value={addressToCheck} onChange={(e) => setAddressToCheck(e.target.value)}/></div>
+      <div><button onClick={checkOwner}>owner check</button></div>
+      <div>{ownerAddress}</div>
     </>
   )
 }
